4aa3596fb0261e020abc10bf455458252b0093d7
[bawar khalid] <bawarkhalid912@gmail.com>
2024-07-09 23:07:25 +0300

    removed validation form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,34 +1,29 @@
 <?php
+
 namespace App\Http\Controllers\Auth;
+
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\auth\registerRequest;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+
 class RegisterController extends Controller
 {
-    public function register(Request $request)
+    public function register(registerRequest $request)
     {
-        $validator = Validator::make($request->all(), [
-            'full_name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-           
-            'address' => 'required|string|max:255',
-            'age' => 'required|integer|min:1',
-            'gender' => 'required|string|in:male,female,other'
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
-        }
+
         $user = User::create([
             'full_name' => $request->full_name,
             'email' => $request->email,
             'address' => $request->address,
+            'password' => $request->password,
             'age' => $request->age,
             'gender' => $request->gender,
         ]);
 
-        // Return a success response
+
         return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
     }
 }

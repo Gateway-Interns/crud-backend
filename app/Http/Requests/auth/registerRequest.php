cb7776935cec5cfd5c672f4a663d34c910e649c9
[bawar khalid] <bawarkhalid912@gmail.com>
2024-07-09 23:07:37 +0300

    created a separated form for the validation

--- app/Http/Requests/auth/registerRequest.php
+++ app/Http/Requests/auth/registerRequest.php
@@ -11,7 +11,7 @@ class registerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class registerRequest extends FormRequest
         return [
             'full_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-
+            'password' => 'required|string|min:8|confirmed',
             'address' => 'required|string|max:255',
             'age' => 'required|integer|min:1',
             'gender' => 'required|string|in:male,female,other'

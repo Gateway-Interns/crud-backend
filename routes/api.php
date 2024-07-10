3418712878975687fd73925b3798ff67fc3dac64
Eman Jamal <emanjse@gmail.com>
2024-07-09 17:15:10 -0700

    Added check-auth route

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\AuthCheckController;
 
 Route::post('login', [LoginController::class, 'login']);
+Route::middleware('auth:sanctum')->post('check-auth', [AuthCheckController::class, 'checkAuth']);
 
 /* Route::get('/user', function (Request $request) {
      return $request->user();

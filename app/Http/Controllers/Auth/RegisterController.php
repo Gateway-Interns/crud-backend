b91acddec1d715fe7c7e21c44f160fcbcda79449
[bawar khalid] <bawarkhalid912@gmail.com>
2024-07-10 23:49:38 +0300

    assigning register resource in Register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\auth\registerRequest;
+use App\Http\Resources\Auth\registerResource;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
@@ -25,6 +26,7 @@ class RegisterController extends Controller
         ]);
 
 
-        return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+        //return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+        return new registerResource($user);
     }
 }

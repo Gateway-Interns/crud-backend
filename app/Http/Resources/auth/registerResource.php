c9fab8366bc896f4c92ec8039ef48a20d592c49a
bawar <bawarkhalid912@gmail.com>
2024-07-11 10:27:44 +0300

    RegsiterResource name changed

--- app/Http/Resources/auth/registerResource.php
+++ app/Http/Resources/auth/registerResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\auth;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class registerResource extends JsonResource
+class RegisterResource extends JsonResource
 {
     public function toArray(Request $request): array
     {

83e18a33f026f410d86a9130dd36bc40b301b499
Eman Jamal <emanjse@gmail.com>
2024-07-14 11:50:25 -0700

    Added postsByUser method with pagination

--- app/Http/Controllers/Auth/PostController.php
+++ app/Http/Controllers/Auth/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\User;
 use App\Http\Resources\Auth\PostResource;
 
 class PostController extends Controller
@@ -11,5 +12,10 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return new PostResource($post);
+    }
+    public function postsByUser(User $userId)
+    {
+       $posts = $userId->posts()->Paginate(4);
+        return PostResource::collection($posts);
     }
 }

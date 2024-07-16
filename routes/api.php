de4c33a200e5f15a1a77060c89b802906639bdf4
Firdaws Abdulla <firdaws.hayder@gmail.com>
2024-07-16 11:24:31 +0300

    changes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
-use App\Http\Controllers\Auth\AuthCheckController;
-use App\Http\Controllers\Auth\PostController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DeleteController;
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', [LoginController::class, 'login']);
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/check-auth', [AuthCheckController::class, 'checkAuth']);
-    Route::get('/posts/{post}', [PostController::class, 'show']);
-});
+Route::delete('/user/{id}',[App\Http\Controllers\DeleteController::class,'deletebyid']);
+
+
+///
+
+///

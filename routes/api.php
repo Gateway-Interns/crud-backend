442fd204d44ebc36c582c88a313062cb480a1b34
[bawar khalid] <bawarkhalid912@gmail.com>
2024-07-17 13:25:15 +0300

    removed index

--- routes/api.php
+++ routes/api.php
@@ -8,12 +8,12 @@ use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisterController::class, 'register']);
-Route::post('login', [LoginController::class, 'login']);
-Route::get('/index', [PostController::class, 'index']);
+
+
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('login', [LoginController::class, 'login']);
     Route::get('/check-auth', [AuthCheckController::class, 'checkAuth']);
-    Route::get('/index', [PostController::class, 'index']);
     Route::get('/posts/{post}', [PostController::class, 'show']);
     Route::post('/store', [PostController::class, 'store']);
     Route::patch('/update/{id}', [PostController::class, 'update']);

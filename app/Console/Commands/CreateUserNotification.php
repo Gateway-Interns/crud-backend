9f948bf5e0523b368f5735ac7db29afbc7ff9c29
[bawar khalid] <bawarkhalid912@gmail.com>
2024-07-17 23:33:03 +0300

    user notification

---
+++ app/Console/Commands/CreateUserNotification.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use App\Notifications\NewReleaseNotification;
+
+class CreateUserNotification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:new-release';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'send notification to user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $users = User::all();
+        foreach ($users as $user) {
+            $user->notify(new NewReleaseNotification());
+        }
+        $this->info('new notf sent to all users.');
+    }
+}

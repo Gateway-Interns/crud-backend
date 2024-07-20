ce8cf43a818e4c1fc5a7ea05ece09821175d7172
Eman Jamal <emanjse@gmail.com>
2024-07-19 22:55:58 -0700

    Update SendVerificationEmail job

--- app/Jobs/SendVerificationEmail.php
+++ app/Jobs/SendVerificationEmail.php
@@ -2,29 +2,38 @@
 
 namespace App\Jobs;
 
+use App\Models\User;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\VerifyEmailNotification;
 
 class SendVerificationEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $user;
+
     /**
      * Create a new job instance.
+     *
+     * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
      * Execute the job.
+     *
+     * @return void
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+        Notification::send($this->user, new VerifyEmailNotification($this->user));
     }
 }

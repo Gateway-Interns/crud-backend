9143a20500fc50dbd6bd277f1ea3ed9db4b8a159
[bawar khalid] <bawarkhalid912@gmail.com>
2024-07-21 17:11:27 +0300

    removed user_id, just skip plz

--- database/migrations/2024_07_17_153850_create_notifications_table.php
+++ database/migrations/2024_07_17_153850_create_notifications_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('type');
             $table->morphs('notifiable');
             $table->text('data');

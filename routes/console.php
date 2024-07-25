6da5906090cf193630345ed366de447b9bbf7895
bawar <bawarkhalid912@gmail.com>
2024-07-25 11:33:26 +0300

    conflicted with notification fixed (idkwhy)

--- routes/console.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function () {
-    $this->comment('hm?');
-})->purpose('Display an inspiring quote')->hourly();

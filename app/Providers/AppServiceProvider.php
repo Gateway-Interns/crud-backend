ad6ed6ece7349522c752eba495485bb66c1a6821
Eman Jamal <emanjse@gmail.com>
2024-07-30 02:22:34 -0700

    Add custom simplepagination macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-        Builder::macro('pagination', function($limit = null, $page = null, $additional = []) {
+        Builder::macro('pagination', function ($limit = null, $page = null, $additional = []) {
             $page = $page ?: LengthAwarePaginator::resolveCurrentPage();
             $limit = $limit ?: 2;
             $total = $this->count();
@@ -37,6 +37,22 @@ class AppServiceProvider extends ServiceProvider
                 $page,
                 array_merge([
                     'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => 'page',
+                ], $additional)
+            );
+        });
+
+        Builder::macro('simplePagination', function ($limit = null, $page = null, $additional = []) {
+            $page = $page ?: Paginator::resolveCurrentPage();
+            $limit = $limit ?: 3;
+            $results = $this->forPage($page, $limit)->get();
+
+            return new Paginator(
+                $results,
+                $limit,
+                $page,
+                array_merge([
+                    'path' => Paginator::resolveCurrentPath(),
                     'pageName' => 'page',
                 ], $additional)
             );

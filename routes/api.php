d3f9b50c3408fb9845c07eb0681d18792b4d1c00
Eman Jamal <emanjse@gmail.com>
2024-08-11 12:06:06 -0700

    Fix LogoutController namespace issue

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 
 
-use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthCheckController;
